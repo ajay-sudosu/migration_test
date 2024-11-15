35065bbc7f819785b2ef4bd9a7de9174358f880a
ajay-netweb <ajay.b@netwebindia.com>
2024-11-15 12:28:21 +0530

    initial commit

---
+++ models.py
@@ -0,0 +1,23 @@
+# models.py
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    String,
+    DateTime,
+)
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False)
+    password = Column(String(100), nullable=False)
+    # email = Column(String(100), nullable=False)
